18bebbb197c946bce2d3dc4933dbdd58fe79a3a5
Jamie Little <jamie.littledv@gmail.com>
2025-10-28 22:32:39 +0000

    Added EF Core references and preliminary entity classes

--- src/efcore-consoleapp/EntityModels/NorthwindDbContext.cs
+++ src/efcore-consoleapp/EntityModels/NorthwindDbContext.cs
@@ -1,4 +1,7 @@
+using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
+using Northwind.EntityModels;
 
 namespace efcore_consoleapp.EntityModels;
 
@@ -6,11 +9,31 @@ public class NorthwindDb : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        // Play nicely with the base class
         base.OnConfiguring(optionsBuilder);
 
         string databaseName = "Northwind.db";
         string path = Path.Combine(Environment.CurrentDirectory, databaseName);
         string connectionString = $"Data Source = {path}";
         optionsBuilder.UseSqlite(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        // Example of usign Fluent API instead of attributes
+        modelBuilder.Entity<Category>()
+            .Property(e => e.CategoryName)
+            .IsRequired()
+            .HasMaxLength(15);
+
+        // Some Sqlite specific configuration.
+        if (Database.ProviderName?.Contains("Sqlite") ?? false)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(e => e.ProductName)
+                .HasConversion<double>();
+        }
     }
 }
