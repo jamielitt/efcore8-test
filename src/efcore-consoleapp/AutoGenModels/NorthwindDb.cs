872d9daf9a3b6e39ec4799e5c38d47f2e4a70317
Jamie Little <jamie.littledv@gmail.com>
2025-10-31 13:00:07 +0000

    Added examples to control logging in T4 template

--- src/efcore-consoleapp/AutoGenModels/NorthwindDb.cs
+++ src/efcore-consoleapp/AutoGenModels/NorthwindDb.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace efcore_consoleapp.AutoGen;
 
@@ -23,7 +24,9 @@ public partial class NorthwindDb : DbContext
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
     {
         optionsBuilder.UseSqlite("Data Source=Northwind.db");
-        optionsBuilder.LogTo(WriteLine)
+
+        // This can be modified by adding in the event to filter on:   
+        optionsBuilder.LogTo(WriteLine, new[] { RelationalEventId.CommandExecuting })
 #if DEBUG
             .EnableSensitiveDataLogging() // Include SQL parameters only in debug
             .EnableDetailedErrors()
