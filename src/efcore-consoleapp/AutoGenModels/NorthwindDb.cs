7cc89899fb90cf00491febc9a12d63f24d3d1475
jamielitt <jamie.littledv@gmail.com>
2025-10-31 12:38:28 +0000

    Added T4 templates for AutoGen (#2)

--- src/efcore-consoleapp/AutoGenModels/NorthwindDb.cs
+++ src/efcore-consoleapp/AutoGenModels/NorthwindDb.cs
@@ -20,7 +20,7 @@ public partial class NorthwindDb : DbContext
     public virtual DbSet<Product> Products { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https: //go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
     {
         optionsBuilder.UseSqlite("Data Source=Northwind.db");
         optionsBuilder.LogTo(WriteLine)
@@ -28,9 +28,8 @@ public partial class NorthwindDb : DbContext
             .EnableSensitiveDataLogging() // Include SQL parameters only in debug
             .EnableDetailedErrors()
 #endif
-            ;
-    }
-
+    ;
+}
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Category>(entity =>
